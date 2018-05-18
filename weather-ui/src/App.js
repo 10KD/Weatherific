7ab4b192d8d097b17f8d0ce2ec45a40b75dc39b3
10kd <mkim.don@gmail.com>
2018-05-18 13:27:08 -0700

    initial commit

---
+++ weather-ui/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import WeatherDisplay from './components/WeatherDisplay';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <WeatherDisplay />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ weather-ui/src/components/WeatherDisplay.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+
+class WeatherDisplay extends React.Component {
+    
+    constructor() {
+        super();
+
+        this.state = {
+            forecast: [],
+        };
+    }
+
+    componentDidMount() {
+        this.getForecast();
+    }
+    
+    getForecast = () => {
+        
+        axios.get('https://www.metaweather.com/api/location/2487956/')
+        .then((response) => {
+            this.setState({ forecast: response.data });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log(this.state)}
+            </div>
+        );
+    }
+}
+
+export default WeatherDisplay;
