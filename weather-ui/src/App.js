0f7e94cde11f97a0b805d939d217840f78a853a3
10kd <mkim.don@gmail.com>
2018-05-21 10:53:18 -0700

    finished app

--- weather-ui/src/App.js
+++ weather-ui/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import WeatherDisplay from './components/WeatherDisplay';
+import WeatherHome from './components/WeatherHome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <WeatherDisplay />
+        <WeatherHome />
       </div>
     );
   }
--- weather-ui/src/components/WeatherDisplay.js
+++ weather-ui/src/components/WeatherDisplay.js
@@ -1,34 +1,75 @@
 import React from 'react';
-import axios from 'axios';
 
 class WeatherDisplay extends React.Component {
-    
-    constructor() {
-        super();
 
-        this.state = {
-            forecast: [],
-        };
-    }
-
-    componentDidMount() {
-        this.getForecast();
+    capitalizeEach = (string) => {
+        let splitString = string.split(" ")
+        let capitalizedString = splitString.map((word) => {
+            let firstLetter = word.charAt(0).toUpperCase()
+            return firstLetter + word.slice(1) + " "
+        })
+        return capitalizedString.join(" ")
     }
     
-    getForecast = () => {
-        let zip = 94112;
-        axios.get('https://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial')
-        .then((response) => {
-            this.setState({ forecast: response.data });
-        });
+    getIcon = (iconName) => {
+        let url = "https://openweathermap.org/img/w/" + iconName + ".png"
+        return <img src={url}/>
     }
 
     render() {
-        return (
-            <div>
-                {JSON.stringify(this.state)}
-            </div>
-        );
+        if (this.props.forecast.name) {
+            if (this.props.error) {
+                return (
+                    <div className="weather-display error">
+                        <p>Error! please enter a valid city name or US zip code.</p>
+                     
+                        <p>{this.props.error}</p>
+                    </div>
+                );
+            } else {
+
+                return (
+                    <div className="weather-display">
+                        <div className="weather-column">
+                            <p>
+                                {"City: " + this.props.forecast.name}
+                            </p>
+                            <p>
+                                {"Temp: " + this.props.forecast.main.temp + " °F"}
+                            </p>    
+                            <p>
+                                {"High: " + this.props.forecast.main.temp_max + " °F"}
+                            </p>        
+                            <p>
+                                {"Low: " + this.props.forecast.main.temp_min + " °F"}  
+                            </p>
+                        </div>
+                        <div className="weather-column">
+                            <p>
+                                {"Description: " + this.capitalizeEach(this.props.forecast.weather[0].description)}
+                            </p> 
+                            <p>
+                                {"Humidity: " + this.props.forecast.main.humidity + "%"}
+                            </p>
+                            <p>
+                                {"Pressure: " + this.props.forecast.main.pressure + " hpa"}
+                            </p>
+                            <p>
+                                {"Wind Speed: " + this.props.forecast.wind.speed + " m/h"}
+                            </p>
+                        </div>
+                        <div className="weather-column icon">
+                            {this.getIcon(this.props.forecast.weather[0].icon)}
+                        </div>
+                    </div>
+                );
+            }
+           
+        } else {
+            return (
+                <div className="weather-display"></div>
+            );
+        }
     }
 }
 
